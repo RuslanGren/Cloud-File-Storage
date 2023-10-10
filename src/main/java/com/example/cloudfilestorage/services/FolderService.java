26f4e63decd7dfadfcca5da4779b658ba9730c85
Ruslan <gren.ruslan@gmail.com>
2023-10-10 21:04:25 +0300

    add

--- src/main/java/com/example/cloudfilestorage/services/FolderService.java
+++ src/main/java/com/example/cloudfilestorage/services/FolderService.java
@@ -3,6 +3,7 @@ package com.example.cloudfilestorage.services;
 import com.example.cloudfilestorage.domain.file.Folder;
 
 public interface FolderService {
+
     void createRootFolder(String name);
 
     void createSubFolder(String name, String path);
