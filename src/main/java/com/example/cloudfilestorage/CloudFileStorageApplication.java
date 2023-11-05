906a37cbf2ffad7340b96ac2226f0709281890c0
Ruslan <gren.ruslan@gmail.com>
2023-11-05 15:44:04 +0200

    add

--- src/main/java/com/example/cloudfilestorage/CloudFileStorageApplication.java
+++ src/main/java/com/example/cloudfilestorage/CloudFileStorageApplication.java
@@ -2,10 +2,12 @@ package com.example.cloudfilestorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableRedisHttpSession
 public class CloudFileStorageApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/cloudfilestorage/domain/user/User.java
+++ src/main/java/com/example/cloudfilestorage/domain/user/User.java
@@ -5,12 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
